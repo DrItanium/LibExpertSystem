5b9c3e8b7fb0bef04a67d887f630428ad089acb1
Joshua Scoggins <Theoretically.x64@gmail.com>
2012-09-16 17:26:05 -0700

    Added the MultifieldBuilder class
    The objective of this class is to make it really easy to build multifields

---
+++ MultifieldBuilder.cpp
@@ -0,0 +1,66 @@
+#include "MultifieldBuilder.h"
+unsigned MultifieldBuilder::getCount() {
+   return count; 
+}
+void* getMultifieldPointer() {
+   return multifieldPointer;
+}
+void MultifieldBuilder::setType(unsigned index, int type) {
+   SetMFType(multifieldPointer, index, type);
+}
+void MultifieldBuilder::setValue(unsigned index, void* value) {
+   SetMFValue(multifieldPointer, index, value);
+}
+void MultifieldBuilder::setSlot(unsigned index, int type, void* value) {
+   setType(index, type);
+   setValue(index, value);
+}
+void MultifieldBuilder::setSlot(unsigned index, PointerAddress value) {
+   setSlot(index, INTEGER, AddLong(value));
+}
+void MultifieldBuilder::setSlot(unsigned index, long value) {
+   setSlot(index, INTEGER, value);
+}
+void MultifieldBuilder::setSlot(unsigned index, int value) {
+   setSlot(index, INTEGER, value);
+}
+void MultifieldBuilder::setSlot(unsigned index, unsigned value) {
+   setSlot(index, INTEGER, value);
+}
+void MultifieldBuilder::setSlot(unsigned index, float value) {
+   setSlot(index, FLOAT, AddDouble(value));
+}
+void MultifieldBuilder::setSlot(unsigned index, double value) {
+   setSlot(index, FLOAT, AddDouble(value));
+}
+void MultifieldBuilder::setSlot(unsigned index, char* v, bool isStringType) {
+   if(isStringType) {
+      setSlot(index, STRING, AddSymbol(v)); 
+   } else {
+      setSlot(index, SYMBOL, AddSymbol(v));
+   }
+}
+
+void MultifieldBuilder::setSlot(unsigned index, const char* v, bool isStringType) {
+   setSlot(index, (char*)v, isStringType);
+}
+
+void MultifieldBuilder::setSlot(unsigned index, std::string v, bool isStringType) {
+   setSlot(index, (char*)v.c_str(), isStringType);
+}
+void MultifieldBuilder::setSlot(unsigned index, llvm::StringRef v, bool isStringType) {
+   setSlot(index, (char*)v.data(), isStringType);
+}
+void MultifieldBuilder::setTrue(unsigned index) {
+   setSlot(index, "TRUE", false);
+}
+void MultifieldBuilder::setFalse(unsigned index) {
+   setSlot(index, "FALSE", false);
+}
+
+void MultifieldBuilder::bindToDataObjectPointer(DATA_OBJECT_PTR ptr) {
+   SetpType(ptr, MULTIFIELD);
+   SetpValue(ptr, multifieldPointer); 
+   SetpDOBegin(ptr, 1);
+   SetpDOEnd(ptr, count);
+}
---
+++ MultifieldBuilder.h
@@ -0,0 +1,63 @@
+#ifndef _multifield_builder_h
+#define _multifield_builder_h
+#include <string>
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/Casting.h"
+#include "llvm/Instruction.h"
+#include "llvm/Instructions.h"
+#include "llvm/IntrinsicInst.h"
+#include "llvm/Argument.h"
+#include "llvm/Analysis/RegionInfo.h"
+#include "llvm/Support/InstIterator.h"
+#include "llvm/User.h"
+#include "llvm/Value.h"
+#include "llvm/Type.h"
+#include "llvm/BasicBlock.h"
+#include "llvm/Analysis/RegionIterator.h"
+#include "llvm/Operator.h"
+#include "llvm/Metadata.h"
+#include "llvm/InlineAsm.h"
+#include "llvm/Argument.h"
+#include "llvm/Constant.h"
+#include "llvm/Constants.h"
+#include "llvm/GlobalVariable.h"
+#include "llvm/GlobalValue.h"
+#include "llvm/GlobalAlias.h"
+#include "llvm/CodeGen/PseudoSourceValue.h"
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/Operator.h"
+#include "llvm/Function.h"
+#include "llvm/Analysis/LoopInfo.h"
+#include "FunctionNamer.h"
+extern "C" {
+#include "clips.h"
+}
+using namespace llvm;
+class MultifieldBuilder {
+   private:
+      void* multifieldPointer;
+      unsigned count;
+   public:
+      MultifieldBuilder(unsigned size) {
+         multifieldPointer = CreateMultifield(size);
+         count = size;
+      }
+      unsigned getCount();
+      void* getMultifieldPointer();
+      void setType(unsigned index, int type);
+      void setValue(unsigned index, void *value);
+      void setSlot(unsigned index, int type, void* value);
+      void setSlot(unsigned index, PointerAddress value);
+      void setSlot(unsigned index, long value);
+      void setSlot(unsigned index, int value);
+      void setSlot(unsigned index, unsigned value);
+      void setSlot(unsigned index, float value);
+      void setSlot(unsigned index, double value);
+      void setSlot(unsigned index, char* v, bool isStringType = false);
+      void setSlot(unsigned index, const char* v, bool isStringType = false);
+      void setSlot(unsigned index, std::string string, bool isStringType = false);
+      void setSlot(unsigned index, llvm::StringRef ref, bool isStringType = false);
+      void setTrue(unsigned index);
+      void setFalse(unsigned index);
+      void bindToDataObjectPointer(DATA_OBJECT_PTR ptr);
+}
