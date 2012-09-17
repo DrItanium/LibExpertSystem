6e2fa4328181c9be720c9d8f8905ca21a6f3b702
Joshua Scoggins <Theoretically.x64@gmail.com>
2012-09-16 18:39:03 -0700

    Updated BasicBlockConstruction.h to reflect changes

--- BasicBlockConstruction.h
+++ BasicBlockConstruction.h
@@ -1,47 +1,51 @@
 #ifndef _clips_basic_block_builder_h
 #define _clips_basic_block_builder_h
 #include "ConstructionTools.h"
+#include<iterator>
 
 class CLIPSBasicBlockBuilder : public CLIPSValueBuilder {
    public:
       CLIPSBasicBlockBuilder(std::string nm, FunctionNamer& namer) : CLIPSValueBuilder(nm, "BasicBlock", namer) { }
-      void addFields(BasicBlock* bb, char* parent, bool constructInstructions = true) {
-         CLIPSValueBuilder::addFields((Value*)bb, parent);
+      void setFields(BasicBlock* bb, char* parent, bool constructInstructions = true) {
+         CLIPSValueBuilder::setFields((Value*)bb, parent);
          char* name = (char*)bb->getName().data();
          FunctionNamer& namer = getNamer();
          namer.registerBasicBlock(name);
-         if(bb->isLandingPad()) addTrueField("IsLandingPad");
-         if(bb->hasAddressTaken()) addTrueField("HasAddressTaken");
+         if(bb->isLandingPad()) setFieldTrue("IsLandingPad");
+         if(bb->hasAddressTaken()) setFieldTrue("HasAddressTaken");
          pred_iterator pi = pred_begin(bb), pe = pred_end(bb);
          if(pi != pe) {
-            openField("Predecessors");
-            for(; pi != pe; ++pi) {
+            unsigned size = std::distance(pi, pe);
+            MultifieldBuilder b0(size);
+            for(unsigned index = 1; pi != pe; ++pi, ++index) {
                BasicBlock* pred = *pi;
-               appendValue(pred->getName());
+               b0.setSlot(index, pred->getName());
             }
-            closeField();
+            setField("Predecessors", &b0);
          }
          succ_iterator si = succ_begin(bb), se = succ_end(bb);
          if(si != se) {
-            openField("Successors");
-            for(; si != se; ++si) {
+            MultifieldBuilder b1(bb->getTerminator()->getNumSuccessors());
+            //openField("Successors");
+            for(unsigned index = 1; si != se; ++si, ++index) {
                BasicBlock* succ = *si;
-               appendValue(succ->getName());
+               b1.setSlot(index, succ->getName());
+             //  appendValue(succ->getName());
             }
-            closeField();
+            setField("Successors", &b1);
+            //closeField();
          }
          if(constructInstructions && !bb->empty()) {
-            std::string tmp;
-            raw_string_ostream data(tmp);
-            openField("Contents");
-            for(BasicBlock::iterator i = bb->begin(), e = bb->end(); i != e; ++i) {
+            unsigned size = (unsigned)bb->getInstList().size();
+            MultifieldBuilder builder(size);
+            unsigned index = 1;
+            for(BasicBlock::iterator i = bb->begin(), e = bb->end(); i != e; ++i, ++index) {
                Instruction* inst = i;
                std::string res = Route(inst, name, namer);
-               appendValue(res);
-               data << " " << res;
+               builder.setSlot(index, res);
             }
-            closeField();
-            addField("Produces", data.str()); 
+            setField("Contents", &builder);
+            setField("Produces", &builder);
          }
       }
 };
