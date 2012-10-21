a80f3cfbe733db267fd6fa9914c8c03a3305f020
Joshua Scoggins <Theoretically.x64@gmail.com>
2012-10-20 22:20:16 -0700

    Removed the commented sections from the code

--- RegionObjectConstruction.h
+++ RegionObjectConstruction.h
@@ -29,12 +29,9 @@ class CLIPSRegionBuilder : public CLIPSObjectBuilder {
 			if(exit != NULL) addField("Exits", exit->getName());
 			char* name = (char*)getName().c_str();
 			FunctionNamer& namer = getNamer();
-			//std::set<std::string> contents;
-			//std::map<std::string, std::set<BasicBlock*> > directFacts;
          //open contents 
 			openField((char*)"Contents");
 			for(Region::element_iterator i = region->element_begin(), e = region->element_end(); i != e; ++i) {
-				//it's really simple we just take a look and 
 				RegionNode* rn = *i;
 				if(!rn->isSubRegion()) {
 					BasicBlock* bb = rn->getEntry(); 
@@ -42,98 +39,10 @@ class CLIPSRegionBuilder : public CLIPSObjectBuilder {
 				} else {
 					Region* subRegion = rn->getNodeAs<Region>();
                appendValue(Route(subRegion, name, namer, getLoopInfo()));
-					//contents.insert(Route(subRegion, name, namer, getLoopInfo()));
 				}
          }
          closeField();
 
-               /*
-					if(namer.pointerRegistered((PointerAddress)bb)) {
-						Loop* target = region->outermostLoopInRegion(getLoopInfo(), bb);
-						//this means that it's part of a loop or we are at a
-						//top-level region
-						if(target == 0) {
-							//add it to the list of block ownership 
-							//this will cause a fact to be asserted
-							std::string loopName = namer.nameFromPointer((PointerAddress)li->getLoopFor(bb));
-							if(directFacts.count(loopName) == 0) {
-								std::set<BasicBlock*> set;
-								set.insert(bb);
-								directFacts[loopName] = set;
-							} else {
-								std::set<BasicBlock*>& set = directFacts[loopName];
-								set.insert(bb);
-							}
-						} else {
-							contents.insert(namer.nameFromPointer((PointerAddress)target));
-						}
-					} else {
-						contents.insert(Route(bb, name, namer));
-					}
-				} else {
-					Region* subRegion = rn->getNodeAs<Region>();
-					contents.insert(Route(subRegion, name, namer, getLoopInfo()));
-				}
-			}
-			if(!directFacts.empty()) {
-				if(!region->isTopLevelRegion()) {
-					IncrementGCLocks();
-					void* templatePtr;
-					//only do this once!
-					templatePtr = FindDeftemplate((char*)"ControlModification"); 
-					DATA_OBJECT relinquishElement;
-					DATA_OBJECT nameElement;
-					relinquishElement.type = SYMBOL;
-					relinquishElement.value = AddSymbol((char*)"Relinquish");
-					nameElement.type = SYMBOL;
-					nameElement.value = AddSymbol((char*)(getName().c_str()));
-					for(std::map<std::string, std::set<BasicBlock*> >::iterator i = directFacts.begin(), e = directFacts.end(); i != e; ++i) {
-						//let's get a little creative here!
-						//I have defined a template of ControlModification
-						void* newFact;
-						void* theMultifield;
-						DATA_OBJECT theValue;
-						newFact = CreateFact(templatePtr);
-						if(newFact == NULL) {
-							llvm::errs() << "Warning!!! Unable to create ControlModification fact!\n";
-						} else {
-							PutFactSlot(newFact, (char*)"ModificationType", &relinquishElement);
-							PutFactSlot(newFact, (char*)"To", &nameElement);
-							theValue.type = SYMBOL;
-							theValue.value = AddSymbol((char*)(i->first).c_str());
-							PutFactSlot(newFact, (char*)"From", &theValue);
-							std::set<BasicBlock*>& set = i->second;
-							unsigned index = 1;
-							theMultifield = CreateMultifield(set.size());
-							for(std::set<BasicBlock*>::iterator j = set.begin(), k = set.end(); j != k; ++j, ++index) {
-								BasicBlock* bb = (*j);
-								char* value = (char*)bb->getName().data();
-								SetMFType(theMultifield, index, SYMBOL);
-								SetMFValue(theMultifield, index, AddSymbol(value));
-							}
-							SetDOBegin(theValue, 1);
-							SetDOEnd(theValue, set.size());
-							theValue.type = MULTIFIELD;
-							theValue.value = theMultifield;
-							PutFactSlot(newFact, (char*)"Subject", &theValue);
-							Assert(newFact);
-						}
-					}
-					DecrementGCLocks();
-				} else {
-					for(std::map<std::string, std::set<BasicBlock*> >::iterator i = directFacts.begin(), e = directFacts.end(); i != e; ++i) {
-						contents.insert(i->first);
-					}
-				}
-			}
-			if(!contents.empty()) {
-				openField((char*)"Contents");
-				for(std::set<std::string>::iterator a = contents.begin(), b = contents.end(); a != b; ++a) {
-					appendValue(*a);
-				}
-				closeField();
-			} 
-         */
 		}
 };
 
