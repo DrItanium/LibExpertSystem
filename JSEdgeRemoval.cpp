040ffe6086b03123bbb5f3adc8e5e39bed5b07b3
Joshua Scoggins <Theoretically.x64@gmail.com>
2012-10-22 18:56:52 -0700

    Removed code specific to my thesis in the form of my JSEdgeRemovalPass and my WavefrontScheduling pass

--- JSEdgeRemoval.cpp
+++
@@ -1,5 +0,0 @@
-#include "JSEdgeRemoval.h"
-
-char JSEdgeRemoval::ID = 0;
-static RegisterPass<JSEdgeRemoval> js("jsedge", "JS Edge Removal", false, false);
-INITIALIZE_PASS(JSEdgeRemoval, "jsedge", "JS Edge Removal", false, false)
