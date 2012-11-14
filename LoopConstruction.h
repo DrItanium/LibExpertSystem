39ef8c16f584e7e48f668afd8461f484557dece7
Joshua Scoggins <Theoretically.x64@gmail.com>
2012-11-14 12:10:50 -0800

    Updated LoopConstruction.h to get _unique_ exit blocks
    This was a fix that was ported over from the merging shift branch.

--- LoopConstruction.h
+++ LoopConstruction.h
@@ -13,7 +13,7 @@ class CLIPSLoopBuilder : public CLIPSObjectBuilder {
          BasicBlock* loopPredecessor = loop->getLoopPredecessor();
          BasicBlock* loopPreheader = loop->getLoopPreheader();
          SmallVector<BasicBlock*, 128> exitBlocks;
-         loop->getExitBlocks(exitBlocks);
+         loop->getUniqueExitBlocks(exitBlocks);
          addField("Depth", loop->getLoopDepth());
          addField("BackEdgeCount", loop->getNumBackEdges());
          if(latch != 0) addField("LatchBlock", header->getName()); 
