5b61dca396913222a884a572d7b5eb6301d91de2
Joshua Scoggins <Theoretically.x64@gmail.com>
2012-12-07 15:20:12 -0800

    Separated the EnvironmentCreationPass into header and code
    This allows other passes to reference it

--- EnvironmentCreationPass.cpp
+++ EnvironmentCreationPass.cpp
@@ -1,84 +1,55 @@
-#include "llvm/Pass.h"
-#include "llvm/Function.h"
-#include "llvm/Analysis/LoopInfo.h"
-#include "llvm/Analysis/RegionInfo.h"
-#include "llvm/Support/raw_ostream.h"
-#include "llvm/ADT/DenseSet.h"
-#include "PassDependencies.h"
-extern "C" {
-#include "clips.h"
-}
+#include "EnvironmentCreationPass.h"
 
 using namespace llvm;
-namespace {
-	typedef long long ExecutionDuration;
-	struct EnvironmentConstruction : public FunctionPass {
-		static char ID;
-		void* environment;
-		public:
-		EnvironmentConstruction() : FunctionPass(ID) {
-
+bool EnvironmentConstruction::environmentCreated() { return (environment != 0); }
+void* EnvironmentConstruction::getEnvironment() { return environment; }
+void EnvironmentConstruction::reset() { 
+	if(environment) {
+		EnvReset(environment); 
+	}
+}
+void EnvironmentConstruction::clear() {
+	if(environment) {
+		EnvClear(environment);
+	}
+}
+void EnvironmentConstruction::run(ExecutionDuration count) {
+	if(environment) {
+		if(count <= 0L) {
+			EnvRun(environment, -1L);
+		} else {
+			EnvRun(environment, count);
 		}
-		~EnvironmentConstruction() {
-			if(environment) {
-				DestroyEnvironment(environment);
-			}
-		}
-		bool environmentCreated() { return (environment != 0); }
-		void* getEnvironment() { return environment; }
-		void resetEnvironment() { 
-			if(environment) {
-				EnvReset(environment); 
-			}
-		}
-		void clearEnvironment() {
-			if(environment) {
-				EnvClear(environment);
-			}
-		}
-		void runEnvironment(ExecutionDuration count) {
-			if(environment) {
-				if(count <= 0L) {
-					EnvRun(environment, -1L);
-				} else {
-					EnvRun(environment, count);
-				}
-			}
-		}
-		void saveInstances(char* fileName) {
-			if(environment) {
-			  EnvSaveInstances(environment, fileName, VISIBLE_SAVE, NULL, TRUE);
-			}
-		}
-		void assertFact(char* str) {
-			if(environment) {
-				EnvAssertString(environment, str);
-			}
-		}
-		bool makeInstances(char* str) {
-			if(environment) {
-				return EnvLoadInstancesFromString(environment, str, -1) != -1;
-			} else {
-				return false;
-			}
-		}
-		bool makeInstance(char* str) {
-			if(environment) {
-				return (EnvMakeInstance(environment, str) != NULL);
-			} else {
-				return false;
-			}
-		}
-		void batchStar(char* str) {
-			if(environment) {
-				EnvBatchStar(environment, str);
-			}
-		}
-		virtual bool runOnFunction(Function& fn) {
-			environment = CreateEnvironment();
-			return false;
-		}
-	};
+	}
+}
+void EnvironmentConstruction::saveInstances(char* fileName) {
+	if(environment) {
+		EnvSaveInstances(environment, fileName, VISIBLE_SAVE, NULL, TRUE);
+	}
+}
+void EnvironmentConstruction::assertFact(char* str) {
+	if(environment) {
+		EnvAssertString(environment, str);
+	}
+}
+bool EnvironmentConstruction::makeInstances(char* str) {
+	if(environment) {
+		return EnvLoadInstancesFromString(environment, str, -1) != -1;
+	} else {
+		return false;
+	}
+}
+bool EnvironmentConstruction::makeInstance(char* str) {
+	if(environment) {
+		return (EnvMakeInstance(environment, str) != NULL);
+	} else {
+		return false;
+	}
+}
+void EnvironmentConstruction::batchStar(char* str) {
+	if(environment) {
+		EnvBatchStar(environment, str);
+	}
 }
 char EnvironmentConstruction::ID = 0;
 INITIALIZE_PASS_BEGIN(EnvironmentConstruction, "environment", "environment constructor", false, false)
---
+++ EnvironmentCreationPass.h
@@ -0,0 +1,45 @@
+#ifndef _environment_creation_pass_h
+#define _environment_creation_pass_h
+#include "llvm/Pass.h"
+#include "llvm/Function.h"
+#include "llvm/Analysis/LoopInfo.h"
+#include "llvm/Analysis/RegionInfo.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/ADT/DenseSet.h"
+#include "PassDependencies.h"
+extern "C" {
+#include "clips.h"
+}
+
+using namespace llvm;
+namespace {
+	typedef long long ExecutionDuration;
+	struct EnvironmentConstruction : public FunctionPass {
+		static char ID;
+		void* environment;
+		public:
+		EnvironmentConstruction() : FunctionPass(ID) {
+
+		}
+		~EnvironmentConstruction() {
+			if(environment) {
+				DestroyEnvironment(environment);
+			}
+		}
+		bool environmentCreated(); 
+		void* getEnvironment();
+		void reset();
+		void clear();
+		void run(ExecutionDuration count);
+		void saveInstances(char* fileName);
+		void assertFact(char* str);
+		bool makeInstances(char* str);
+		bool makeInstance(char* str);
+		void batchStar(char* str);
+		virtual bool runOnFunction(Function& fn) {
+			environment = CreateEnvironment();
+			return false;
+		}
+	};
+}
+#endif
