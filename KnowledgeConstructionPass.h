7f82c460716f16b488df7a8fab978d5eac4f480f
Joshua Scoggins <Theoretically.x64@gmail.com>
2012-12-07 22:56:14 -0800

    Added some debug statements to test KnowledgeConstructionPass.h

--- KnowledgeConstructionPass.h
+++ KnowledgeConstructionPass.h
@@ -49,21 +49,30 @@ namespace llvm {
 
 		public:
 		KnowledgeConstruction() : FunctionPass(ID) {
+			//initializeKnowledgeConstructionPass(*PassRegistry::getPassRegistry());
+			llvm::errs() << "Initialized KnowledgeConstruction\n";
 			std::string tmp;
 			instances = new llvm::DenseMap<PointerAddress, std::string>();
 			instanceStream = new llvm::raw_string_ostream(tmp);
+			llvm::errs() << "Finished Initializing KnowledgeConstruction\n";
 		}
 		~KnowledgeConstruction();
+
 		void resetInstanceStream() {
 			instanceStream->flush();
 		}
 		llvm::DenseMap<PointerAddress, std::string>* getInstances() { return instances; }
 		std::string getInstancesAsString() { return instanceStream->str(); }
 
+		virtual void print(raw_ostream &OS, const Module *) const {
+			OS << "Instances:\n";
+			OS << instanceStream->str() << '\n';
+			OS << "End Instances\n";
+		}
 		virtual void getAnalysisUsage(AnalysisUsage &Info) const {
 			Info.addRequired<LoopInfo>();
 			Info.addRequired<RegionInfo>();
-			Info.addRequired<FunctionNamer>();
+			Info.addRequiredTransitive<FunctionNamer>();
 			//we don't need the environment yet and it's actually smarter to just
 			//wait until the custom pass so that the programmer has complete
 			//control over the code
@@ -92,6 +101,7 @@ namespace llvm {
 		void route(LoopInfo& li, FunctionNamer& namer, char* parent);
 		void updateFunctionContents(Function& fn, FunctionNamer& namer);
 		virtual bool runOnFunction(Function& fn) {
+			llvm::errs() << "Called runOnFunction of KnowledgeConstruction\n";
 			char* funcName;
 			//get the function namer object
 			LoopInfo &loops = getAnalysis<LoopInfo>();
