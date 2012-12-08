c9ac08fd7579d21ebf5e3bb0b79882d0a53dc3cc
Joshua Scoggins <Theoretically.x64@gmail.com>
2012-12-07 16:14:30 -0800

    Started writing the runOnFunction method for KnowledgeConstruction.h

--- KnowledgeConstructionPass.h
+++ KnowledgeConstructionPass.h
@@ -7,10 +7,14 @@
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/ADT/DenseSet.h"
 #include "KnowledgeConstructionEngine.h"
-#include "LLVMWrapper.h"
-#include "JSEdgeRemoval.h"
 #include "PassDependencies.h"
 #include "EnvironmentCreationPass.h"
+#include "FunctionNamer.h"
+#include "llvm/ADT/DenseSet.h"
+#include "llvm/ADT/DenseMap.h"
+#include "llvm/Value.h"
+#include <string>
+#include <map>
 extern "C" {
   #include "clips.h"
 }
@@ -20,16 +24,29 @@ using namespace llvm;
 namespace {
    struct KnowledgeConstruction : public FunctionPass {
       static char ID;
+		llvm::DenseMap<PointerAddress, std::string>* instances;
       public:
-      KnowledgeConstruction() : FunctionPass(ID) { }
+      KnowledgeConstruction() : FunctionPass(ID) {
+			instances = new llvm::DenseMap<PointerAddress, std::string>();
+		}
       virtual void getAnalysisUsage(AnalysisUsage &Info) const {
          Info.addRequired<LoopInfo>();
          Info.addRequired<RegionInfo>();
-			//Info.addRequired<EnvironmentConstruction>();
+			Info.addRequired<FunctionNamer>();
+			//we don't need the environment yet and it's actually smarter to just
+			//wait until the custom pass so that the programmer has complete
+			//control over the code
       }
 		//TODO: add the route commands
-
+	
       virtual bool runOnFunction(Function& fn) {
+			//get the function namer object
+			LoopInfo &loops = getAnalysisUsage<LoopInfo>();
+			RegionInfo &regions = getAnalysisUsage<RegionInfo>();
+			FunctionNamer &fn = getAnalysisUsage<FunctionNamer>();
+			fn->reset();
+			instances->clear();
+
 			//EnvironmentConstruction &env = getAnalysisUsage<EnvironmentConstruction>();
 			return false;
       }
