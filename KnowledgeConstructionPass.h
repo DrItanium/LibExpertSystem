91e75ff052f3d59634c4adcd7bcc6dfb6a83e208
Joshua Scoggins <Theoretically.x64@gmail.com>
2012-12-07 16:57:43 -0800

    Added instance commands for routing to KnowledgeConstructionPass.h

--- KnowledgeConstructionPass.h
+++ KnowledgeConstructionPass.h
@@ -1,11 +1,33 @@
 #ifndef _knowledge_construction_pass_h
 #define _knowledge_construction_pass_h
 #include "llvm/Pass.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/Casting.h"
+#include "llvm/Instruction.h"
+#include "llvm/Instructions.h"
+#include "llvm/IntrinsicInst.h"
+#include "llvm/Argument.h"
+#include "llvm/Analysis/RegionInfo.h"
+#include "llvm/Support/InstIterator.h"
+#include "llvm/User.h"
+#include "llvm/Value.h"
+#include "llvm/Type.h"
+#include "llvm/BasicBlock.h"
+#include "llvm/Analysis/RegionIterator.h"
+#include "llvm/Operator.h"
+#include "llvm/Metadata.h"
+#include "llvm/InlineAsm.h"
+#include "llvm/Argument.h"
+#include "llvm/Constant.h"
+#include "llvm/Constants.h"
+#include "llvm/GlobalVariable.h"
+#include "llvm/GlobalValue.h"
+#include "llvm/GlobalAlias.h"
+#include "llvm/CodeGen/PseudoSourceValue.h"
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/Operator.h"
 #include "llvm/Function.h"
 #include "llvm/Analysis/LoopInfo.h"
-#include "llvm/Analysis/RegionInfo.h"
-#include "llvm/Support/raw_ostream.h"
-#include "llvm/ADT/DenseSet.h"
 #include "KnowledgeConstructionEngine.h"
 #include "PassDependencies.h"
 #include "EnvironmentCreationPass.h"
@@ -16,40 +38,61 @@
 #include <string>
 #include <map>
 extern "C" {
-  #include "clips.h"
+#include "clips.h"
 }
 
 using namespace llvm;
 //char nilObject[41] = "(nil of ConstantPointerNull (Pointer 0))";
 namespace {
-   struct KnowledgeConstruction : public FunctionPass {
-      static char ID;
+	struct KnowledgeConstruction : public FunctionPass {
+		static char ID;
 		llvm::DenseMap<PointerAddress, std::string>* instances;
-      public:
-      KnowledgeConstruction() : FunctionPass(ID) {
+		public:
+		KnowledgeConstruction() : FunctionPass(ID) {
 			instances = new llvm::DenseMap<PointerAddress, std::string>();
 		}
-      virtual void getAnalysisUsage(AnalysisUsage &Info) const {
-         Info.addRequired<LoopInfo>();
-         Info.addRequired<RegionInfo>();
+		~KnowledgeConstruction();
+		llvm::DenseMap<PointerAddress, std::string>* getInstances() { return instances; }
+		virtual void getAnalysisUsage(AnalysisUsage &Info) const {
+			Info.addRequired<LoopInfo>();
+			Info.addRequired<RegionInfo>();
 			Info.addRequired<FunctionNamer>();
 			//we don't need the environment yet and it's actually smarter to just
 			//wait until the custom pass so that the programmer has complete
 			//control over the code
-      }
+		}
 		//TODO: add the route commands
-	
-      virtual bool runOnFunction(Function& fn) {
+
+		std::string route(Value* val, char* parent, FunctionNamer& namer);
+		std::string route(Value* val, FunctionNamer& namer);
+		std::string route(User* user, char* parent, FunctionNamer& namer);
+		std::string route(Constant* cnst, FunctionNamer& namer);
+		std::string route(Constant* cnst, char* parent, FunctionNamer& namer);
+		std::string route(Instruction* inst, char* parent, FunctionNamer& namer);
+		std::string route(Type* t, FunctionNamer& namer);
+		std::string route(Operator* op, char* parent, FunctionNamer& namer);
+		std::string route(BasicBlock* bb, char* parent, FunctionNamer& namer, bool constructInstructions = true);
+		std::string route(Region* region, char* parent, FunctionNamer& namer);
+		std::string route(Argument* arg, char* parent, FunctionNamer& namer);
+		std::string route(Loop* loop, char* parent, FunctionNamer& namer);
+		void route(RegionInfo& ri, char* parent, FunctionNamer& namer);
+		void route(LoopInfo& li, char* parent, FunctionNamer& namer);
+		void updateFunctionContents(Function& fn, FunctionNamer& namer);
+		virtual bool runOnFunction(Function& fn) {
+			char* funcName;
 			//get the function namer object
 			LoopInfo &loops = getAnalysisUsage<LoopInfo>();
 			RegionInfo &regions = getAnalysisUsage<RegionInfo>();
-			FunctionNamer &fn = getAnalysisUsage<FunctionNamer>();
+			FunctionNamer &namer = getAnalysisUsage<FunctionNamer>();
+			funcName = (char*)fn.getName().data();
 			fn->reset();
 			instances->clear();
-
-			//EnvironmentConstruction &env = getAnalysisUsage<EnvironmentConstruction>();
+			updateFunctionContents(fn, namer);
+			route(loops, funcName, namer);
+			route(regions, funcName, namer);
+			//at this point we shall have a set of instance strings
 			return false;
-      }
-   };
+		}
+	};
 }
 #endif
