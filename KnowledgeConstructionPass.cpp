b2f8ba6cf38792a549f8dae6e5f32aa66f4dfd2d
Joshua Scoggins <Theoretically.x64@gmail.com>
2012-12-07 18:14:32 -0800

    Completed route functions for MDNode, MDString, Argument, and InlineASM

--- KnowledgeConstructionPass.cpp
+++ KnowledgeConstructionPass.cpp
@@ -31,25 +31,157 @@ std::string KnowledgeConstruction::route(Value* val, FunctionNamer& namer, char*
 		} else if(MDString* mds = dyn_cast<MDString>(val)) {
 			return route(mds, namer, parent);
 		} else {
-         llvm::errs() << "WARNING: Found an unimplemented value " << *val << '\n';
+			llvm::errs() << "WARNING: Found an unimplemented value " << *val << '\n';
 			char* gensymBuffer = (char*)calloc(64, sizeof(char));
 			namer.makeGensymID(gensymBuffer);
 			std::string name(gensymBuffer);
 			free(gensymBuffer);
-         CLIPSValueBuilder vb (name, "LLVMValue", namer);
-         vb.open();
-         vb.addFields(val, parent);
-         vb.close();
+			CLIPSValueBuilder vb (name, "LLVMValue", namer);
+			vb.open();
+			vb.addFields(val, parent);
+			vb.close();
 			std::string& str = vb.getCompletedString();
 			addToKnowledgeBase((PointerAddress)val, str);
-         return name;
+			return name;
 		}
 	}
 }
 
-std::string route(Value* val, FunctionNamer& namer) {
+std::string KnowledgeConstruction::route(Value* val, FunctionNamer& namer) {
+	if(Instruction* inst = dyn_cast<Instruction>(val)) {
+		return route(inst, namer, (char*)inst->getParent()->getName().data());
+	} else {
+		return route(val, namer, "nil");
+	}
+}
+
+std::string KnowledgeConstruction::route(User* user, FunctionNamer& namer, char* parent) {
 
 }
+std::string KnowledgeConstruction::route(Constant* cnst, FunctionNamer& namer) {
+
+}
+std::string KnowledgeConstruction::route(Constant* cnst, FunctionNamer& namer, char* parent) {
+
+}
+std::string KnowledgeConstruction::route(Instruction* inst, FunctionNamer& namer, char* parent) {
+
+}
+std::string KnowledgeConstruction::route(Type* t, FunctionNamer& namer) {
+
+}
+std::string KnowledgeConstruction::route(Operator* op, FunctionNamer& namer, char* parent) {
+
+}
+std::string KnowledgeConstruction::route(BasicBlock* bb, FunctionNamer& namer, char* parent, bool constructInstructions) {
+
+}
+std::string KnowledgeConstruction::route(Region* region, FunctionNamer& namer, char* parent) {
+
+}
+std::string KnowledgeConstruction::route(Argument* arg, FunctionNamer& namer, char* parent) {
+	if(namer.pointerRegistered((PointerAddress)arg)) {
+		return namer.nameFromPointer((PointerAddress)arg);
+	} else {
+		char* gensymBuffer = (char*)calloc(64, sizeof(char));
+		namer.makeGensymID(gensymBuffer);
+		std::string name(gensymBuffer);
+		free(gensymBuffer);
+		CLIPSArgumentBuilder ab(name, namer);
+		ab.open();
+		ab.addFields(name, parent);
+		ab.clone();
+		std::string &str = ab.getCompletedString();
+		addToKnowledgeBase((PointerAddress)arg, str);
+		return name;
+	}
+}
+std::string KnowledgeConstruction::route(Loop* loop, FunctionNamer& namer, char* parent) {
+
+}
+std::string KnowledgeConstruction::route(MDString* mds, FunctionNamer& namer, char* parent) {
+	if(namer.pointerRegistered((PointerAddress)mds)) {
+		return namer.nameFromPointer((PointerAddress)mds);
+	} else {
+		char* gensymBuffer = (char*)calloc(64, sizeof(char));
+		namer.makeGensymID(gensymBuffer);
+		std::string name(gensymBuffer);
+		free(gensymBuffer);
+		CLIPSValueBuilder vb(name, "LLVMMDString", namer);
+		vb.open();
+		vb.addFields(val, parent);
+		vb.addStringField("String", mds->getString());
+		vb.close();
+		std::string &str = vb.getCompletedString();
+		addToKnowledgeBase((PointerAddress)mds, str);
+		return name;
+	}
+
+}
+std::string KnowledgeConstruction::route(MDNode* mdn, FunctionNamer& namer, char* parent) {
+	if(namer.pointerRegistered((PointerAddress)mdn)) {
+		return namer.nameFromPointer((PointerAddress)mdn);
+	} else {
+		char* gensymBuffer = (char*)calloc(64, sizeof(char));
+		namer.makeGensymID(gensymBuffer);
+		std::string name(gensymBuffer);
+		free(gensymBuffer);
+		CLIPSValueBuilder vb (name, "LLVMMDNode", namer);
+		vb.open();
+		vb.addFields(val, parent);
+		if(mdn->isFunctionLocal()) vb.addTrueField("IsFunctionLocal");
+		const Function* fn = mdn->getFunction();
+		if(fn != 0) {
+			vb.addField("TargetFunction", fn->getName());
+		}
+		unsigned total = mdn->getNumOperands();
+		if(total > 0) {
+			char* cName = (char*)name.c_str();
+			vb.openField("Operands");
+			for(unsigned i = 0; i < total; ++i) {
+				vb.appendValue(route(mdn->getOperand(i), namer, cName));
+			}
+			vb.closeField();
+		}
+		vb.close();
+		std::string &str = vb.getCompletedString();
+		addToKnowledgeBase((PointerAddress)mdn, str);
+		return name;
+	}
+}
+std::string KnowledgeConstruction::route(InlineAsm* iasm, FunctionNamer& namer, char* parent) {
+	if(namer.pointerRegistered((PointerAddress)iasm)) {
+		return namer.nameFromPointer((PointerAddress)iasm);
+	} else {
+		char* gensymBuffer = (char*)calloc(64, sizeof(char));
+		namer.makeGensymID(gensymBuffer);
+		std::string name(gensymBuffer);
+		free(gensymBuffer);
+		CLIPSValueBuilder vb (name, "LLVMInlineAsm", namer);
+		vb.open();
+		vb.addFields(val, parent);
+		if(iasm->hasSideEffects()) vb.addTrueField("HasSideEffects");
+		if(iasm->isAlignStack()) vb.addTrueField("IsAlignStack");
+		const std::string& aStr = iasm->getAsmString();
+		const std::string& cnStr = iasm->getConstraintString();
+		vb.addStringField("AsmString", aStr);
+		vb.addStringField("ConstraintString", cnStr);
+		vb.close();
+		std::string &str = vb.getCompletedString();
+		addToKnowledgeBase((PointerAddress)iasm, str);
+		return name;
+	}
+}
+void KnowledgeConstruction::route(RegionInfo& ri, FunctionNamer& namer, char* parent) {
+
+}
+void KnowledgeConstruction::route(LoopInfo& li, FunctionNamer& namer, char* parent) {
+
+}
+void KnowledgeConstruction::updateFunctionContents(Function& fn, FunctionNamer& namer) {
+
+}
+
 char KnowledgeConstruction::ID = 0;
 static RegisterPass<KnowledgeConstruction> kc("knowledge", "Knowledge constructor", false, false);
 INITIALIZE_PASS(KnowledgeConstruction, "knowledge", "Knowledge constructor", false, false)
