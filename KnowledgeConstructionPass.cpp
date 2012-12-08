20050d1f1401b6a43036d7be22f2f3f8e44f7826
Joshua Scoggins <Theoretically.x64@gmail.com>
2012-12-07 16:14:14 -0800

    Modified the opt registration name for KnowledgeConstructionPass.cpp

--- KnowledgeConstructionPass.cpp
+++ KnowledgeConstructionPass.cpp
@@ -1,6 +1,5 @@
 #include "KnowledgeConstructionPass.h"
 char KnowledgeConstruction::ID = 0;
-INITIALIZE_PASS_BEGIN(KnowledgeConstruction, "knowledge", "Knowledge constructor", false, false)
-INITIALIZE_PASS_END(KnowledgeConstruction, "knowledge", "Knowledge constructor", false, false)
+static RegisterPass<KnowledgeConstruction> kc("knowledge", "Knowledge constructor", false, false);
+INITIALIZE_PASS(KnowledgeConstruction, "knowledge", "Knowledge constructor", false, false)
 //for opt
-static RegisterPass<KnowledgeConstruction> X("knowledge", "Knowledge constructor", false, false);
